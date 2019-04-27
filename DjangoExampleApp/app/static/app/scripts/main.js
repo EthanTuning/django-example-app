d279816be95f601981abf828af67c961faa00a37
Ethan Tuning <etuning5@hotmail.com>
2019-04-27 08:47:26 -0700

    finished everything up

--- DjangoExampleApp/app/static/app/scripts/main.js
+++ DjangoExampleApp/app/static/app/scripts/main.js
@@ -2,6 +2,15 @@ $(document).ready(init);
 
 function init() {
 
+    menuBurgerClick();
 
+}
+
+function menuBurgerClick() {
+
+    $(".navbar-burger").click(function () {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
 
 }
